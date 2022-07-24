f01b08147cf2cdac965947123990ac55a41a922d
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-24 12:31:30 +0300

    Modifed hammerspoon's window.lua file

--- .hammerspoon/window.lua
+++ .hammerspoon/window.lua
@@ -1,14 +1,14 @@
 -- half of screen
-hs.hotkey.bind({'alt', 'cmd'}, 'h', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1}) end)
-hs.hotkey.bind({'alt', 'cmd'}, 'l', function() hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1}) end)
-hs.hotkey.bind({'alt', 'cmd'}, 'k', function() hs.window.focusedWindow():moveToUnit({0, 0, 1, 0.5}) end)
-hs.hotkey.bind({'alt', 'cmd'}, 'j', function() hs.window.focusedWindow():moveToUnit({0, 0.5, 1, 0.5}) end)
+hs.hotkey.bind({'alt', 'cmd'}, 'left', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1}) end)
+hs.hotkey.bind({'alt', 'cmd'}, 'right', function() hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1}) end)
+hs.hotkey.bind({'alt', 'cmd'}, 'up', function() hs.window.focusedWindow():moveToUnit({0, 0, 1, 0.5}) end)
+hs.hotkey.bind({'alt', 'cmd'}, 'down', function() hs.window.focusedWindow():moveToUnit({0, 0.5, 1, 0.5}) end)
 
 -- quarter of screen
-hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'h', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 0.5}) end)
-hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'l', function() hs.window.focusedWindow():moveToUnit({0.5, 0.5, 0.5, 0.5}) end)
-hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'k', function() hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 0.5}) end)
-hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'j', function() hs.window.focusedWindow():moveToUnit({0, 0.5, 0.5, 0.5}) end)
+hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'left', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 0.5}) end)
+hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'right', function() hs.window.focusedWindow():moveToUnit({0.5, 0.5, 0.5, 0.5}) end)
+hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'up', function() hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 0.5}) end)
+hs.hotkey.bind({'shift', 'alt', 'cmd'}, 'down', function() hs.window.focusedWindow():moveToUnit({0, 0.5, 0.5, 0.5}) end)
 
 -- full screen
 hs.hotkey.bind({'alt', 'cmd'}, 'f', function() hs.window.focusedWindow():moveToUnit({0, 0, 1, 1}) end)
